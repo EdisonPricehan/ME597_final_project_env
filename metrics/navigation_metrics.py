1ea2a33935ca6ea3ec1ae049ceb9ec0c8e18a3d7
Edison <princehan920453024@gmail.com>
2023-04-28 01:07:14 -0400

    improve metrics for navigation tasks; added 5 fixed goal poses.

--- metrics/navigation_metrics.py
+++ metrics/navigation_metrics.py
@@ -3,13 +3,24 @@
 import rospy
 from gazebo_msgs.msg import ContactsState
 from gazebo_msgs.msg import ModelStates
-from geometry_msgs.msg import PoseStamped
-from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import PoseStamped, Pose, Quaternion
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import math
 
 
+GOAL_POSES = [[1, 0, -0.8], [6, 0, 0.8], [8.5, -2, -1.57], [-4, 2, 3.14], [-4.5, -3, -1.57]]
+
+
 class NavigationMetrics:
-    def __init__(self):
+    def __init__(self, automatic: bool = False):
+        # whether to use hard-coded list of goal poses to automatically assign to the robot
+        self.automatic = automatic
+        if self.automatic:
+            assert len(GOAL_POSES) != 0, "No goal poses are given!"
+            self.goal_pose_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+            self.cur_goal_idx = 0
+            self.finished = False
+
         # collision related variables
         self.collision_counts = 0
         self.last_collision_time = None
@@ -18,7 +29,7 @@ class NavigationMetrics:
         # navigation time related variables
         self.navigation_start_time = None
         self.navigation_duration = 0
-        self.NAVIGATION_UNFINISHED_TIME = 1000
+        self.NAVIGATION_TIMEOUT_SEC = 60
 
         # report as a dictionary of all navigation metrics
         self.report = {'start_poses': [], 'goal_poses': [], 'time_costs': [], 'collision_counts': []}
@@ -45,6 +56,7 @@ class NavigationMetrics:
         rospy.loginfo(f"Navigation reports for {len(self.report['start_poses'])} run(s):")
         rospy.loginfo(f"time costs: {self.report['time_costs']}")
         rospy.loginfo(f"collision counts: {self.report['collision_counts']}")
+        rospy.loginfo(f"Total navigation time in sec: {sum(self.report['time_costs'])}, total collision counts: {sum(self.report['collision_counts'])}")
 
     def reset(self, reset_goal: bool = False):
         self.collision_counts = 0
@@ -66,10 +78,14 @@ class NavigationMetrics:
             rospy.loginfo_throttle(1, f"Your current collision counts: {self.collision_counts}.")
 
     def goal_callback(self, msg):
+        if self.automatic:
+            rospy.logwarn(f"No manual goal pose is accepted in automatic grading mode!")
+            return
+
         # current navigation is preempted, metrics should be differentiable
         if self.goal_pose is not None:
             rospy.logwarn("Current navigation is not finished, but new goal comes, preempt!")
-            self.report['time_costs'].append(self.navigation_duration + self.NAVIGATION_UNFINISHED_TIME)
+            self.report['time_costs'].append(self.navigation_duration + self.NAVIGATION_TIMEOUT_SEC)
             self.report['collision_counts'].append(self.collision_counts)
 
         rospy.loginfo("New goal received!")
@@ -86,6 +102,15 @@ class NavigationMetrics:
     @staticmethod
     def dist(x1: float, y1: float, x2: float, y2: float):
         return math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
+
+    @staticmethod
+    def pose2d_to_rospose(x, y, yaw):
+        rospose = Pose()
+        rospose.position.x = x
+        rospose.position.y = y
+        q = quaternion_from_euler(0, 0, yaw)
+        rospose.orientation = Quaternion(*q)
+        return rospose
 
     def reached_goal(self):
         if self.goal_pose is None:
@@ -113,6 +138,11 @@ class NavigationMetrics:
         return True
 
     def robot_callback(self, msg):
+        # won't do anything if all goal poses are finished
+        if self.finished:
+            return
+        cur_goal_failed = False
+
         # update robot pose
         robot_id = msg.name.index(self.robot_name)
         self.robot_pose = msg.pose[robot_id]
@@ -121,19 +151,40 @@ class NavigationMetrics:
         if self.goal_pose is not None:
             assert self.navigation_start_time is not None
             self.navigation_duration = (rospy.Time.now() - self.navigation_start_time).to_sec()
+            # timeout then select the next goal pose if there is any
+            if self.navigation_duration > self.NAVIGATION_TIMEOUT_SEC:
+                rospy.logwarn("Navigation timeout, preempt!")
+                cur_goal_failed = True
 
-        # if reached goal, fill the report for this session, then reset variables
-        if self.reached_goal():
+        elif self.automatic:
+            self.goal_pose = self.pose2d_to_rospose(*GOAL_POSES[self.cur_goal_idx])
+            rospy.loginfo(f"Publishing goal pose {self.goal_pose}!")
+            goal_pose_stamped = PoseStamped()
+            goal_pose_stamped.header.stamp = rospy.Time.now()
+            goal_pose_stamped.pose = self.goal_pose
+            self.goal_pose_pub.publish(goal_pose_stamped)
+            self.navigation_start_time = rospy.Time.now()
+            self.report['start_poses'].append(self.robot_pose)
+            self.report['goal_poses'].append(self.goal_pose)
+            self.reset()
+
+        # if reached goal or timeout, fill the report for this session, then reset variables
+        if self.reached_goal() or cur_goal_failed:
             self.report['time_costs'].append(self.navigation_duration)
             self.report['collision_counts'].append(self.collision_counts)
             self.reset(reset_goal=True)
-            rospy.loginfo(f"Navigation finished successfully, waiting for new goal...")
+            rospy.loginfo(f"Navigation finished, waiting for new goal...")
             self.__call__()
+            if self.automatic:
+                self.cur_goal_idx += 1
+                if self.cur_goal_idx == len(GOAL_POSES):
+                    rospy.loginfo(f"All goal poses are finished!")
+                    self.finished = True
 
 
 if __name__ == '__main__':
     try:
-        nav_metrics = NavigationMetrics()
+        nav_metrics = NavigationMetrics(automatic=True)
     except rospy.ROSInterruptException:
         pass
     finally:
